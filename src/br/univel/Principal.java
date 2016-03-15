f58e50224a763e3edfcaa91717f78ec2465111ce
Flávio Rocha Cezarino <ghostreapedrc@gmail.com>
2016-03-15 20:19:43 -0300

    alteração a pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -10,8 +10,8 @@ package br.univel;
 public class Principal {
 
 	public static void main(String[] args){
-		System.out.println("Olá Mundo!");
-		System.out.println("É Nóis!");
+		System.out.println("Hello World!");
+		System.out.println("it's wi!");
 	}
 	
 }
